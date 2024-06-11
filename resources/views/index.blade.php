7dc757191e263cc02fa9c82eb6ee4c96dd18d0ee
I Wayan Joni <iwyjoni@gmail.com>
2024-06-11 12:58:15 +0800

    Add footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     Test
                 </div>

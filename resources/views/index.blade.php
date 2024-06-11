901e60872d65b4032f61ad5c91eb0eca8a32a1a4
wardana <mint@Macbook14.local>
2024-06-11 12:59:59 +0800

    add leaflet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,24 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" id="map">
                     Test
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    var map = L.map('map').setView([-8.409518, 115.188919], 10);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([-8.409518, 115.188919]).addTo(map)
+        .bindPopup('Bali<br>Island of the Gods')
+        .openPopup();
+
+</script>
 @endsection
